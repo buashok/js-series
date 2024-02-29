98518c1bdf5c88651bae9ad434b5c5c41e8e5089
buashok <161696754+buashok@users.noreply.github.com>
2024-02-29 15:01:58 +0000

    for data type

---
+++ basic/03_conversiondatatype.js
@@ -0,0 +1,10 @@
+// let score = " "
+// let valueInNumber = Number(score);
+// let boolValue = Boolean(score)
+// console.log(boolValue)
+
+/********************************* Operation ********************* */
+let str1 = "hello"
+let str2 = " Ashok"
+let str3 = str1 + str2
+console.log(str3);
