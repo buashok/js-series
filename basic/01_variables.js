5d4dfdd9d05c7c759752ecc9d4ed9726768848fc
buashok <161696754+buashok@users.noreply.github.com>
2024-02-29 08:59:26 +0000

    update variable file

--- basic/01_variables.js
+++ basic/01_variables.js
@@ -2,4 +2,5 @@ const accountId= 455443;
 let accountEmail = "ubashok@outlook.com";
 let accountPassword = "12345";
 const accountCity = "Jaipur"
-    
+// prefer not to use var because of issue in block scope and functional scope
+console.table([accountCity, accountEmail, accountId])
